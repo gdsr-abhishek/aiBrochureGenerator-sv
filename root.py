ab07ec1eb2ccf138aaf46d7671601c20ebda9bde
Abhishek Guntuboina <gdsr.abhishek@gmail.com>
2025-04-05 14:34:31 +0530

    adding website scraping logic,

--- root.py
+++ root.py
@@ -4,24 +4,33 @@ import requests
 from  openai import OpenAI 
 from dotenv import load_dotenv
 import gradio as gr
+from bs4 import BeautifulSoup
 
 def create_brochure(url_to_scrape):
-    result=''
+    result= scrape_content(url_to_scrape)
+    soup = BeautifulSoup(result, 'html.parser')
+    title = soup.title.string if soup.title else "No title found"
+    text= ''
+    links=[]
+    if soup.body:
+        for irrelevant in soup.body(["script", "style", "img", "input"]):
+            irrelevant.decompose()
+        text = soup.body.get_text(separator="\n", strip=True)
+    else:
+        text = ""
+    links = [link.get('href') for link in soup.find_all('a')]
+    links = [link for link in links if link]
+    return f"the scrape result is as follows:\n \
+         {title} \n\n \
+            {text} \n \n \
+                {links} \n \n"
 
-    return result
-
-async def scrape_content(url_to_scrape):
-    async with async_playwright() as p:
-        browser = await p.chromium.launch()
-        page = await browser.new_page()
-        await page.goto(url_to_scrape)
-        content = await page.content() 
-        await browser.close()
-        # print(content)
-        return content
+def scrape_content(url_to_scrape):
+    response = requests.get(url_to_scrape).content
+    return response
 
 app = gr.Interface(
-    fn=scrape_content,
+    fn=create_brochure,
     inputs=["text"],
     outputs=gr.Textbox(),
     theme="gradio/monochrome"
